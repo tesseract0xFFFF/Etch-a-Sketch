0aaee298cc6697f9c9a9045afe27d31ef75e9d1d
tesseractx0FFFF <itkugit321@gmail.com>
2023-03-11 23:09:39 +0200

    added a reset button, row and column numbers change according to user input. added a 100 row / column limit. input also changed css grid properties directly.

--- js/etch.js
+++ js/etch.js
@@ -1,14 +1,44 @@
     // store the container element.
     const containerDiv = document.getElementById("container");
-    
-// creates 16*16 elements
-for (i = 0; i<256; i++){
+
+    let columnNumber = +prompt("Enter the number of columns");
+    let rowNumber = +prompt("Enter the number of rows");
+
+    // sets css grid row and column properties.
+    containerDiv.style.gridTemplateColumns = "repeat(" + columnNumber +", 1fr)";
+    containerDiv.style.gridTemplateRows = "repeat(" + rowNumber +", 1fr)";
 
 
-    const gridElement = document.createElement('div');
-    gridElement.setAttribute("class", "grid-item");
-    gridElement.setAttribute("id", `sq${i}`);
-    containerDiv.appendChild(gridElement);
+
+    // adds a reset button
+    let btn = document.getElementById("resetButton");
+
+    btn.addEventListener("click", () => {
+        let cellList = document.querySelectorAll(".grid-item");
+        cellList.forEach(gridItem => gridItem.style.backgroundColor = "#ccc");
+    })
+
+
+
+
     
+// creates columnNumber*rowNumber elements and adds event listeners to each element.
+if (columnNumber < 100 && rowNumber < 100) {
+    for (i = 0; i<columnNumber*rowNumber; i++){
+
+
+        const gridElement = document.createElement('div');
+        gridElement.setAttribute("class", "grid-item");
+        gridElement.setAttribute("id", `sq${i}`);
+        gridElement.addEventListener("mouseover", () => {
+            gridElement.style.backgroundColor = "black";
+        } )
+        containerDiv.appendChild(gridElement);
+    
+    }
 }
+else {
+    alert("Input must be smaller than 100!");
+}
+
 
